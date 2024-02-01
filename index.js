c69079abdc6f7276327220abe373f67cb67dcc82
Phawat63915 <phawat63915@gmail.com>
2024-02-01 23:23:37 +0700

    tester add

--- index.js
+++ index.js
@@ -27,10 +27,24 @@ app.get('/users', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  let stmt = db.prepare('INSERT INTO users VALUES (?, ?)');
-  stmt.run(req.body.id, req.body.name);
-  stmt.finalize();
-  res.send('User added');
+  let checkStmt = db.prepare('SELECT * FROM users WHERE id = ?');
+  checkStmt.get(req.body.id, (err, row) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (row) {
+      res.status(400).send('User with this ID already exists');
+    } else {
+      let insertStmt = db.prepare('INSERT INTO users VALUES (?, ?)');
+      insertStmt.run(req.body.id, req.body.name, (err) => {
+        if (err) {
+          res.status(500).send(err.message);
+        } else {
+          insertStmt.finalize();
+          res.send('User added');
+        }
+      });
+    }
+  });
 });
 
 app.put('/users/:id', (req, res) => {
